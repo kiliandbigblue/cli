b2b7eb4135935906c6333668e387aef04c546c25
kiliandbigblue <kilian.demeulemeester@bigblue.io>
2024-09-04 10:53:51 +0200

    feature/branch-create

---
+++ create.sh
@@ -0,0 +1,38 @@
+TYPE=$(gum choose "fix" "feature" "chore")
+NAME=$(gum input --placeholder "> Branch name" | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z0-9]/-/g')
+BRANCH_NAME=$TYPE/$NAME
+
+if git ls-remote --heads origin "$BRANCH_NAME" | grep -q "$BRANCH_NAME"; then
+  # Branch exists
+  echo "Branch $BRANCH_NAME already exists on remote"
+  exit 1
+fi
+
+echo "Preparing new branch: $BRANCH_NAME"
+
+if [ "$MODE" = "-i" ]; then
+    # Look for untracked files
+    git status -s | grep -q '^??' && {
+       gum confirm "Untracked files found. Do you want to add them to the commit?" && git add $(git status -s | grep '^??' | cut -c4-) || echo "Untracked files not added to the commit"
+   }
+   # Look for unstaged changes
+    git status -s | grep -q '^ M' && {
+         git add -p .
+    }
+
+else
+    git add .
+fi
+
+# Check that the staging area is not empty
+if git diff --cached --quiet; then
+    echo "No changes to commit"
+    exit 0
+fi
+
+echo "Creating branch $BRANCH_NAME"
+git checkout -b "$BRANCH_NAME"
+git commit -m "$BRANCH_NAME"
+
+exit 0
+
---
+++ justfile
@@ -0,0 +1,28 @@
+
+[private]
+@default:
+    just --list
+
+# Fetch all remotes
+[no-cd]
+[private]
+@fetch-all:
+  git fetch --all
+
+# Deploy a reflow or atlas service interactively 
+[no-cd]
+deploy: fetch-all
+  #!/usr/bin/env sh
+  ~/projects/cli/deploy.sh
+
+# Create a new branch and commit the selected changes
+[no-cd]
+create $MODE='-i': fetch-all
+  #!/usr/bin/env sh
+  ~/projects/cli/create.sh
+  
+# Amend the selected changes to the current commit
+[no-cd]
+modify $MODE='-i': fetch-all
+  #!/usr/bin/env sh
+  ~/projects/cli/modify.sh
