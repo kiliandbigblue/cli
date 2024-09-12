8622004543354648dfbeed9723dcf09f283172e9
kiliandbigblue <kilian.demeulemeester@bigblue.io>
2024-09-12 10:12:59 +0200

    fix/switch-to-gh-poi

--- submit.sh
+++ submit.sh
@@ -3,7 +3,7 @@ CURRENT_BRANCH=$(git branch --show-current)
 git push -f origin $CURRENT_BRANCH
 
 # Check if a PR already exists for the current branch
-PR_URL=$(gh pr view --json url --jq '.url' --state open --base main --head $CURRENT_BRANCH 2>/dev/null)
+PR_URL=$(gh pr view --json url --jq '.url')
 if [ -n "$PR_URL" ]; then
     gh pr view --web
 else 
--- sync.sh
+++ sync.sh
@@ -11,37 +11,39 @@ git checkout $MAIN_BRANCH
 git pull
 
 # Delete all merged branches and squash and merged branches
-git branch --merged $MAIN_BRANCH | grep -v '^\* ' | xargs -n 1 git branch -d
-git for-each-ref refs/heads/ "--format=%(refname:short)" | while read branch; do mergeBase=$(git merge-base $MAIN_BRANCH $branch) && [[ $(git cherry $MAIN_BRANCH $(git commit-tree $(git rev-parse "$branch^{tree}") -p $mergeBase -m _)) == "-"* ]] && git branch -D $branch; done
+gh poi
 
-# Rebase all local branches on top of the main branch, ignoring branches with rebase conflicts.
-# The ignored branches will be left untouched and their name is printed out at the end.
-IGNORED_BRANCHES=()
-branches=$(git branch --format='%(refname:short)' | grep -Ev "(^\*|master|main|dev)")
-
-for branch in $branches; do
-    git checkout "$branch"
-    git pull origin $branch
-    if [ $? -ne 0 ]; then
-        echo "Failed to pull $branch. Skipping..."
-        IGNORED_BRANCHES+=("$branch")
-        continue
-    fi
-    git rebase "$MAIN_BRANCH"
-    if [ $? -ne 0 ]; then
-        echo "Rebase conflict in $branch. Skipping..."
-        git rebase --abort
-        IGNORED_BRANCHES+=("$branch")
-    fi
-done
-
-if [ ${#IGNORED_BRANCHES[@]} -eq 0 ]; then
-    echo "All branches were rebased successfully."
-else
-    echo "All branches were rebased except the following branches: ${IGNORED_BRANCHES[@]}"
-fi
-
-git checkout $MAIN_BRANCH
+# git branch --merged $MAIN_BRANCH | grep -v '^\* ' | xargs -n 1 git branch -d
+# git for-each-ref refs/heads/ "--format=%(refname:short)" | while read branch; do mergeBase=$(git merge-base $MAIN_BRANCH $branch) && [[ $(git cherry $MAIN_BRANCH $(git commit-tree $(git rev-parse "$branch^{tree}") -p $mergeBase -m _)) == "-"* ]] && git branch -D $branch; done
+#
+# # Rebase all local branches on top of the main branch, ignoring branches with rebase conflicts.
+# # The ignored branches will be left untouched and their name is printed out at the end.
+# IGNORED_BRANCHES=()
+# branches=$(git branch --format='%(refname:short)' | grep -Ev "(^\*|master|main|dev)")
+#
+# for branch in $branches; do
+#     git checkout "$branch"
+#     git pull origin $branch
+#     if [ $? -ne 0 ]; then
+#         echo "Failed to pull $branch. Skipping..."
+#         IGNORED_BRANCHES+=("$branch")
+#         continue
+#     fi
+#     git rebase "$MAIN_BRANCH"
+#     if [ $? -ne 0 ]; then
+#         echo "Rebase conflict in $branch. Skipping..."
+#         git rebase --abort
+#         IGNORED_BRANCHES+=("$branch")
+#     fi
+# done
+#
+# if [ ${#IGNORED_BRANCHES[@]} -eq 0 ]; then
+#     echo "All branches were rebased successfully."
+# else
+#     echo "All branches were rebased except the following branches: ${IGNORED_BRANCHES[@]}"
+# fi
+#
+# git checkout $MAIN_BRANCH
 
 exit 0
 
